0aa87aa805f9d637ab2a472e8c4c34bdb88a0c90
hans <focus5n@naver.com>
2022-12-24 19:14:01 +0900

    STD DELETE

--- src/App.js
+++ src/App.js
@@ -51,6 +51,22 @@ function Create(props) {
 }
 
 
+function Update(props) {
+    return <article>
+        <h2>Update</h2>
+        <form onSubmit={event => {
+            event.preventDefault();
+            const title = event.target.title.value;
+            const body = event.target.body.value;
+            props.onUpdate(title, body);
+        }}>
+            <p><input type="text" name="title" placeholder="title"/></p>
+            <p><textarea name="body" placeholder="body"></textarea></p>
+            <p><input type="submit" value="Update"/></p>
+        </form>
+    </article>
+}
+
 function App() {
     // const _mode = useState("WELCOME");
     // const mode = _mode[0];
@@ -67,9 +83,29 @@ function App() {
         link: "https://ko.reactjs.org/blog"
     }, {id: 4, title: "커뮤니티", link: "https://ko.reactjs.org/community/support.html"}]);
     let content;
+    let contextControl = null;
 
     if (mode === "WELCOME") {
         content = <Article title="Welcome!" description="I am Welcome."></Article>;
+        contextControl = <>
+            <li><a id="updateButton" href={"/update/" + nextId} onClick={event => {
+                event.preventDefault();
+                setMode("UPDATE")
+            }}>isUpdate?</a>
+            </li>
+            <li>
+                <input type="button" value="Delete" onClick={() => {
+                    const newTopics = [];
+                    for (let i=0; topics.length; i++) {
+                        if(topics[i] !== nextId) {
+                            newTopics.push(topics[i]);
+                        }
+                    }
+                    setTopics(newTopics);
+                    setMode("WELCOME");
+                }}/>
+            </li>
+        </>;
     } else if (mode === "REACT") {
         content = <Article title="Welcome, React!" description="I am React."></Article>;
     } else if (mode === "CREATE") {
@@ -83,25 +119,37 @@ function App() {
             setMode("WELCOME");
             setNextId(nextId + 1);
         }}></Create>
+    } else if (mode === "UPDATE") {
+        content = <Update title="" body="" onUpdate={(_title, _body) => {
+            const newTopic = {id: nextId, title: _title, body: _body};
+            const newTopics = [...topics];
+            newTopics.push(newTopic);
+            setMode("REACT");
+            setNextId(nextId + 1);
+        }}></Update>
     } else {
         content = <Article title="Welcome!" description="I am Welcome."></Article>;
     }
 
     return (<div className="App">
-            <Header title="React Props" onChangeMode={() => {
-                if (mode === "WELCOME") {
-                    setMode("REACT");
-                } else {
-                    setMode("WELCOME");
-                }
-            }}></Header>
-            <Nav topics={topics}></Nav>
-            {content}
-            <a id="CreateButton" href="/create" onClick={event => {
+        <Header title="React Props" onChangeMode={() => {
+            if (mode === "WELCOME") {
+                setMode("REACT");
+            } else {
+                setMode("WELCOME");
+            }
+        }}></Header>
+        <Nav topics={topics}></Nav>
+        {content}
+        <ul>
+            <li><a id="CreateButton" href="/create" onClick={event => {
                 event.preventDefault();
                 setMode("CREATE");
-            }}>create</a>
-        </div>);
+            }}>isCreate?</a></li>
+            {contextControl}
+        </ul>
+
+    </div>);
 }
 
 export default App;
