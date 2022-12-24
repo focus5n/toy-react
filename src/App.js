e4f85e29092c4ba5a8630ed1a9260ec5022db82e
hans <focus5n@naver.com>
2022-12-24 16:42:05 +0900

    STD CREATE

--- src/App.js
+++ src/App.js
@@ -2,34 +2,33 @@ import './App.css';
 import {useState} from "react";
 
 function Header(props) {
-  return <header className="App-header">
-      <h1><a href='/' onClick={ event => {
-        event.preventDefault();
-        props.onChangeMode();
-      }
-      }>{props.title}</a></h1>
+    return <header className="App-header">
+        <h1><a href='/' onClick={event => {
+            event.preventDefault();
+            props.onChangeMode();
+        }}>{props.title}</a></h1>
     </header>
 }
 
 function Nav(props) {
     const li = [];
 
-    for (let i=0; i < props.topics.length; i++) {
+    for (let i = 0; i < props.topics.length; i++) {
         let topic = props.topics[i];
         li.push(<li key={topic.id}><a href={topic.link}>{topic.title}</a></li>);
     }
 
-  return  <nav className="Nav">
-      <ol>
-          {li}
-      </ol>
+    return <nav className="Nav">
+        <ol>
+            {li}
+        </ol>
     </nav>
 }
 
 function Article(props) {
-  return  <article className="Article">
-      <h2>{props.title}</h2>
-      <p>{props.description}</p>
+    return <article className="Article">
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
     </article>
 }
 
@@ -56,12 +55,15 @@ function App() {
     // const setMode = _mode[1];
     const [mode, setMode] = useState('WELCOME');
     const [nextId, setNextId] = useState(5);
-    const [topics, setTopics] = useState([
-        {id:1, title:"문서", link:"https://ko.reactjs.org/docs/getting-started.html"},
-        {id:2, title:"자습서", link:"https://ko.reactjs.org/tutorial/tutorial.html"},
-        {id:3, title:"블로그", link:"https://ko.reactjs.org/blog"},
-        {id:4, title:"커뮤니티", link:"https://ko.reactjs.org/community/support.html"}
-    ]);
+    const [topics, setTopics] = useState([{
+        id: 1,
+        title: "문서",
+        link: "https://ko.reactjs.org/docs/getting-started.html"
+    }, {id: 2, title: "자습서", link: "https://ko.reactjs.org/tutorial/tutorial.html"}, {
+        id: 3,
+        title: "블로그",
+        link: "https://ko.reactjs.org/blog"
+    }, {id: 4, title: "커뮤니티", link: "https://ko.reactjs.org/community/support.html"}]);
     let content;
 
     if (mode === "WELCOME") {
@@ -70,7 +72,7 @@ function App() {
         content = <Article title="Welcome, React!" description="I am React."></Article>;
     } else if (mode === "CREATE") {
         content = <Create onCreate={(_title, _body) => {
-            const newTopic = {id: nextId, title:_title, body:_body}
+            const newTopic = {id: nextId, title: _title, body: _body}
             const newTopics = [...topics];
             newTopics.push(newTopic);
 
@@ -82,24 +84,21 @@ function App() {
         content = <Article title="Welcome!" description="I am Welcome."></Article>;
     }
 
-  return (
-    <div className="App">
-        <Header title="React Props" onChangeMode={() => {
-            if (mode === "WELCOME") {
-                setMode("REACT");
-            } else {
-                setMode("WELCOME");
-            }
-        }
-        }></Header>
-        <Nav topics={topics}></Nav>
-        {content}
-        <a id="CreateButton" href="/create" onClick={event => {
-            event.preventDefault();
-            setMode("CREATE");
-        }}>create</a>
-    </div>
-  );
+    return (<div className="App">
+            <Header title="React Props" onChangeMode={() => {
+                if (mode === "WELCOME") {
+                    setMode("REACT");
+                } else {
+                    setMode("WELCOME");
+                }
+            }}></Header>
+            <Nav topics={topics}></Nav>
+            {content}
+            <a id="CreateButton" href="/create" onClick={event => {
+                event.preventDefault();
+                setMode("CREATE");
+            }}>create</a>
+        </div>);
 }
 
 export default App;
