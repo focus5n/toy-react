c633a34878303f8e79ae22c3f4c66b71dc0ea11c
hans <focus5n@naver.com>
2022-12-24 15:50:14 +0900

    STD State

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
+import {useState} from "react";
 
 function Header(props) {
   return <header className="App-header">
-      {/*https://ko.reactjs.org/*/}
       <h1><a href='/' onClick={ event => {
         event.preventDefault();
         props.onChangeMode();
       }
       }>{props.title}</a></h1>
     </header>
-}
-
-function Alert() {
-    return alert("I am Header!");
 }
 
 function Nav(props) {
@@ -23,7 +19,7 @@ function Nav(props) {
         li.push(<li key={topic.id}><a href={topic.link}>{topic.title}</a></li>);
     }
 
-  return  <nav>
+  return  <nav className="Nav">
       <ol>
           {li}
       </ol>
@@ -31,25 +27,48 @@ function Nav(props) {
 }
 
 function Article(props) {
-  return  <article>
+  return  <article className="Article">
       <h2>{props.title}</h2>
       <p>{props.description}</p>
     </article>
 }
 
+
+
 function App() {
+    // const _mode = useState("WELCOME");
+    // const mode = _mode[0];
+    // const setMode = _mode[1];
+    const [mode, setMode] = useState('WELCOME');
+
     const topics = [
         {id:crypto.randomUUID(), title:"문서", link:"https://ko.reactjs.org/docs/getting-started.html"},
         {id:crypto.randomUUID(), title:"자습서", link:"https://ko.reactjs.org/tutorial/tutorial.html"},
         {id:crypto.randomUUID(), title:"블로그", link:"https://ko.reactjs.org/blog"},
         {id:crypto.randomUUID(), title:"커뮤니티", link:"https://ko.reactjs.org/community/support.html"}
     ]
+    let content;
+
+    if (mode === "WELCOME") {
+        content = <Article title="Welcome!" description="I am Welcome."></Article>;
+    } else if (mode === "REACT") {
+        content = <Article title="Welcome, React!" description="I am React."></Article>;
+    } else {
+        content = <Article title="Welcome!" description="I am Welcome."></Article>;
+    }
 
   return (
     <div className="App">
-        <Header title="React Props" onChangeMode={Alert}></Header>
+        <Header title="React Props" onChangeMode={() => {
+            if (mode === "WELCOME") {
+                setMode("REACT");
+            } else {
+                setMode("WELCOME");
+            }
+        }
+        }></Header>
         <Nav topics={topics}></Nav>
-        <Article title="Welcome, Props!" description="I am Props."></Article>
+        {content}
     </div>
   );
 }
