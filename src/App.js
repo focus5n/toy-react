fbc727cd50753c9c493d6e2fe5c9977b91f7907f
hans <focus5n@naver.com>
2022-12-24 14:22:36 +0900

    STD Event

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 
-function Header() {
+function Header(props) {
   return <header className="App-header">
-      <h1><a href='https://ko.reactjs.org/'>React</a></h1>
+      {/*https://ko.reactjs.org/*/}
+      <h1><a href='/' onClick={ event => {
+        event.preventDefault();
+        props.onChangeMode();
+      }
+      }>{props.title}</a></h1>
     </header>
 }
 
-function Nav() {
+function Alert() {
+    return alert("I am Header!");
+}
+
+function Nav(props) {
+    const li = [];
+
+    for (let i=0; i < props.topics.length; i++) {
+        let topic = props.topics[i];
+        li.push(<li key={topic.id}><a href={topic.link}>{topic.title}</a></li>);
+    }
+
   return  <nav>
-      <li><a href='https://ko.reactjs.org/docs/getting-started'>문서</a></li>
-      <li><a href='https://ko.reactjs.org/ko.reactjs.org/tutorial/tutorial'>자습서</a></li>
-      <li><a href='https://ko.reactjs.org/blog'>블로그</a></li>
-      <li><a href='https://ko.reactjs.org/community/support'>커뮤니티</a></li>
+      <ol>
+          {li}
+      </ol>
     </nav>
 }
 
-function Article() {
+function Article(props) {
   return  <article>
-      <h2>Welcome</h2>
-      <p>Hello, React!</p>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
     </article>
 }
 
 function App() {
+    const topics = [
+        {id:crypto.randomUUID(), title:"문서", link:"https://ko.reactjs.org/docs/getting-started.html"},
+        {id:crypto.randomUUID(), title:"자습서", link:"https://ko.reactjs.org/tutorial/tutorial.html"},
+        {id:crypto.randomUUID(), title:"블로그", link:"https://ko.reactjs.org/blog"},
+        {id:crypto.randomUUID(), title:"커뮤니티", link:"https://ko.reactjs.org/community/support.html"}
+    ]
+
   return (
     <div className="App">
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+        <Header title="React Props" onChangeMode={Alert}></Header>
+        <Nav topics={topics}></Nav>
+        <Article title="Welcome, Props!" description="I am Props."></Article>
     </div>
   );
 }
