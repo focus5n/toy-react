01fd64f59f650550e3e7e5d34b365f218414ff3f
hans <focus5n@naver.com>
2022-12-24 12:41:25 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return <header className="App-header">
+      <h1><a href='https://ko.reactjs.org/'>React</a></h1>
+    </header>
+}
+
+function Nav() {
+  return  <nav>
+      <li><a href='https://ko.reactjs.org/docs/getting-started'>문서</a></li>
+      <li><a href='https://ko.reactjs.org/ko.reactjs.org/tutorial/tutorial'>자습서</a></li>
+      <li><a href='https://ko.reactjs.org/blog'>블로그</a></li>
+      <li><a href='https://ko.reactjs.org/community/support'>커뮤니티</a></li>
+    </nav>
+}
+
+function Article() {
+  return  <article>
+      <h2>Welcome</h2>
+      <p>Hello, React!</p>
+    </article>
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
